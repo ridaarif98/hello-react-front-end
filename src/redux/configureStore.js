116ec5efd236d9a6796b0a439f254bb2f42adce5
ridaarif98 <ridaarif16@gmail.com>
2022-08-10 17:01:32 +0200

    Setup redux store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,13 @@
+import { applyMiddleware, createStore, combineReducers } from "redux";
+import thunk from "redux-thunk";
+
+import greetingReducer from "./greetingReducer";
+
+const reducer = combineReducers({
+    greeting: greetingReducer
+})
+
+const store = createStore(
+    reducer,applyMiddleware(thunk)
+);
+export default store;
