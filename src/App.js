c16495591b2c0190bff85af99b82464b3d923d73
ridaarif98 <ridaarif16@gmail.com>
2022-08-10 17:03:55 +0200

    Get data from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react';
+import store from './redux/configureStore';
 
 import Greeting from "./Greeting";
 function App() {
   return (
+    <Provider store={store}>
     <Router>
       <Routes>
         <Route path="/" element={<Greeting />}/>
       </Routes>
     </Router>
+    </Provider>
   );
 }
 
